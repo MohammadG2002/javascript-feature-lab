273eb67012f3c91b28edd80160f74d1f6e3fa51b
MohammadG2002 <157503349+MohammadG2002@users.noreply.github.com>
2026-02-20 23:10:43 +0200

    Built repo structure
    - Built repository structure
    
    - Solved 01-variables section

---
+++ JS-Tutorial/01-variables/b-const.js
@@ -0,0 +1,5 @@
+const a = "Hello";
+console.log(a);
+
+a = "World";
+console.log(a); // Error, can't re-assign constant variables
---
+++ JS-Tutorial/01-variables/c-types.js
@@ -0,0 +1,45 @@
+// variable types
+
+// Number
+let a = 1;
+console.log(a);
+
+// BigInt
+a = BigInt(11231242342342341231231231);
+console.log(a);
+
+// String
+a = "Hello";
+console.log(a);
+
+// Boolean
+a = true;
+console.log(a);
+
+// Null
+a = null;
+console.log(a);
+
+// Undefined
+a = undefined;
+console.log(a);
+
+// Object
+a = {};
+console.log(a);
+
+// Array
+a = [];
+console.log(a);
+
+// Date
+a = new Date();
+console.log(a);
+
+// Function
+a = () => {};
+console.log(a);
+
+// Symbol
+a = Symbol("id");
+console.log(a);
