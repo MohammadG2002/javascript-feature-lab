2c1d5a78145ca9a854d8c32658078088cfa231c1
MohammadG2002 <157503349+MohammadG2002@users.noreply.github.com>
2026-02-21 10:56:19 +0200

    Finished if Conditions

---
+++ JS-Tutorial/03-if_conditions/c-switch.js
@@ -0,0 +1,13 @@
+let a = 5;
+
+switch (a) {
+  default:
+    console.log("default");
+    break;
+  case 5:
+    console.log("5");
+    break;
+  case 10:
+    console.log("10");
+    break;
+}
---
+++ JS-Tutorial/03-if_conditions/e-logical.js
@@ -0,0 +1,13 @@
+let a = 15;
+let b = 20;
+
+// &&
+console.log(a < 20 && b > 15); // true
+console.log(a > 20 && b > 15); // false
+
+// ||
+console.log(a < 20 || b > 15); // true
+console.log(a > 20 || b > 15); // true
+
+// !
+console.log(!(a < 20)); // false
